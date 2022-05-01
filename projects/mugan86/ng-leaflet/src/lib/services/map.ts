8a1aeb3b86ec9e61551b86d1eff9eaec7fd3e411
Anartz Mugika Ledo <mugan86@gmail.com>
2022-05-01 22:55:33 +0200

    refactor: Clean unused options

--- projects/mugan86/ng-leaflet/src/lib/services/map.ts
+++ projects/mugan86/ng-leaflet/src/lib/services/map.ts
@@ -1,25 +1,12 @@
 import { Map } from 'leaflet';
-import { tileLayerSelect } from '../config/tile-layers/helpers';
-import { tileLayers } from '../config/tile-layers/ui';
 import { IConfigMap } from '../models/config-map';
-import { IScaleOptions } from '../models/controls';
-import { Controls } from './controls.service';
 
 export class BaseMap {
     private map!: Map;
     private mapId = 'map';
-    private baseLayer = {
-        map: tileLayers.baseLayers.default.map,
-        atribution: tileLayers.baseLayers.default.atribution
-    }
-    private overLayer: { map: string, atribution: string } | undefined;
     private center!: [number, number];
-    private scale!: IScaleOptions | undefined;
-    private config!: IConfigMap | undefined;
     constructor(config?: IConfigMap) {
-        this.config = config || undefined;
         this.center = config && config!!.center || [43.1824528,-2.3878554];
-        this.scale = config && config.scale || undefined;
         this.init();
     }
 
@@ -28,24 +15,8 @@ export class BaseMap {
      */
     private init(): void {
         this.map = new Map(this.mapId).setView(this.center, 10);
-        // this.setLayers();
-        Controls.addBaseOverLayers(this.map)
-        
     }
     get = () => this.map;
-
-    private setLayers() {
-        this.overLayer = this.config && this.config!!.layers!!.overLayers[0] || undefined;
-        tileLayerSelect(this.baseLayer.map, {
-            attribution: this.baseLayer.atribution,
-        }).addTo(this.map);
-
-        if (this.overLayer) {
-            tileLayerSelect(this.overLayer.map, {
-                attribution: this.baseLayer.atribution,
-            }).addTo(this.map);
-        }
-    }
 
     /**
      * Place camera centering and zooming with location points
@@ -55,11 +26,5 @@ export class BaseMap {
         this.get().fitBounds([
             ...markers.map((point) => [point.lat, point.lng] as [number, number]),
         ]);
-    }
-
-    protected setControls() {
-
-    }
-
-    
+    }    
 }
