96542eca6131c91128b3f02f13c2f874746e418e
Anartz Mugika Ledo <mugan86@gmail.com>
2022-05-01 22:55:52 +0200

    feature: Impement control to add base oveer layers option

--- projects/mugan86/ng-leaflet/src/lib/services/controls.service.ts
+++ projects/mugan86/ng-leaflet/src/lib/services/controls.service.ts
@@ -1,44 +1,51 @@
 import { control, ControlPosition, Map } from 'leaflet';
 import { tileLayerSelect } from '../config/tile-layers/helpers';
-import { tileLayers } from '../config/tile-layers/ui';
-import { IScaleOptions } from './../models/controls'
+import { ILayers, IScaleOptions } from './../models/controls'
 class Controls {
     static addScale(map: Map, config?: IScaleOptions) {
         // Vamos a añadir el control de escala
         control.scale(config).addTo(map);
     }
 
-    static addBaseOverLayers(map: Map, position: ControlPosition = 'topright') {
-        // Capas base
-        const defaultTileLayer = tileLayerSelect().addTo(map);
+    /**
+     * Generate layers control to available users want differente layers
+     * to use in maps.
+     * // TODO Need to refactor because in this function we use repetitive functions
+     * @param map Map instantiate object when add control layers
+     * @param layers Layers with base and overlayers to add in map
+     * @param position control position in map
+     */
+    static addBaseOverLayers(map: Map, layers: ILayers, position: ControlPosition = 'topright') {
+        console.log(layers);
+        if (!layers.baseLayers || !layers.overLayers) {
+            throw new Error("Need to add Base and Over Layers");
+        }
 
-        const cycleMapThunderForest = tileLayerSelect(tileLayers.baseLayers.cycloOsm.map);
+        if (layers.baseLayers.length < 2) {
+            console.warn("Take advantage of at least two base layers to take advantage of this feature")
+        }
 
-        const oSMManik = tileLayerSelect(tileLayers.baseLayers.osmManik.map);
+        // Start show layer by default. TODO make dinamically with "default" boolean value
+        const defaultLayer = tileLayerSelect(layers.baseLayers[0].map, {
+            attribution: layers.baseLayers[0].atribution
+        }).addTo(map);
+
+        const baseLayers = layers.baseLayers.reduce((a, layer, index) => {
+            return (index > 0) ?({
+                ...a, [layer.label]: tileLayerSelect(layer.map, {
+                    attribution: layer.atribution
+                })
+            }) : {
+                ...a, [layers.baseLayers[0].label]: defaultLayer // Map Default select layer
+            }
+        }, {} // Start value
+        );
+
+        const overLayers = layers.overLayers.reduce((a, layer) => ({ ...a, [layer.label]: tileLayerSelect(layer.map) }), {});
 
 
-        // Capas de superposición
-
-        const railwayMap = tileLayerSelect(tileLayers.overlayers.openRailway);
-
-        const hiking = tileLayerSelect(tileLayers.overlayers.wayMarkedTrails.hiking);
-
-
-
-        // Control de las capas
-
-        const baseLayers = {
-            'Default': defaultTileLayer,
-            'Cycle Map': cycleMapThunderForest,
-            'OSM Manik': oSMManik
-        };
-
-        const overlays = {
-            'Hiking': hiking,
-            'Railway': railwayMap
-        };
-
-        control.layers(baseLayers, overlays, {
+        // Layers controls
+        control.layers(baseLayers, overLayers, {
             position
         }).addTo(map);
     }
