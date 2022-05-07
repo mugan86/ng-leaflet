63f72d1105af239f97d625f2de46da4e8ab8cb91
Anartz Mugika Ledo <mugan86@gmail.com>
2022-05-07 12:45:57 +0200

    feature: Examples options with dinamic menu

--- src/app/pages/example/example.component.ts
+++ src/app/pages/example/example.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { IConfigMap } from '@mugan86/ng-leaflet';
+import { IConfigMap, tileLayers } from '@mugan86/ng-leaflet';
 import { appModule } from 'src/app/configurations';
+import navigation from './../../../assets/menu.json';
 
 @Component({
   selector: 'app-example',
@@ -13,33 +14,28 @@ export class ExampleComponent implements OnInit {
   items = Array(50).fill({
     item: "banana"
   });
+  menusItems = navigation.menus;
   snippet1 = appModule;
-  configMap: IConfigMap = {
-    center: [27.9881, 86.9253],
-    zoom: {
-      default: 15
-    }
-  }
+  
+  
+  marker = [{'lat': 43.1824528, 'lng': -2.3878554}]
+  randomMarkers = true;
 
-  /*configMap: IConfigMap = {
+
+  configMap: IConfigMap = {
     fullscreen: true,
-    
-    watermark: {
-      position: 'bottomleft',
-      size: '50px'
-    },
     layers: {
       baseLayers: [
         {
           label: 'Default',
           map: tileLayers.baseLayers.default.map,
-          atribution: tileLayers.baseLayers.default.atribution,
-          default: true
+          atribution: tileLayers.baseLayers.default.atribution
         },
         {
           label: 'CycloOSM',
           map: tileLayers.baseLayers.cycloOsm.map,
-          atribution: tileLayers.baseLayers.cycloOsm.atribution
+          atribution: tileLayers.baseLayers.cycloOsm.atribution,
+          default: true // <=== To show with this selection
         },
         {
           label: 'OSMHot',
@@ -59,20 +55,19 @@ export class ExampleComponent implements OnInit {
       {
         label: 'Open Railway',
         map: tileLayers.overlayers.openRailway,
-        select: false
+        select: true
       }]
     },
-    // If add zoom config (if add one property info) default zoom control remove
-    zoom: {
-      position: 'topright',
-      zoomInTitle: 'Acercar',
-      zoomOutTitle: 'Alejar'
-    }
-  }*/
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  getRandomInRange = (from: number, to: number, fixed = 3) => {
+    return +(Math.random() * (to - from) + from).toFixed(fixed) * 1;
+    // .toFixed() returns string, so ' * 1' is a trick to convert to number
+  };
 
   selectOption(id: number) {
     this.option = id;
